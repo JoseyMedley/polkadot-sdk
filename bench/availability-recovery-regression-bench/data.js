9d799db6f6010616ff6cae38726befcb6d0c6543
github-action-benchmark <github@users.noreply.github.com>
2025-06-30 05:09:06 +0000

    add availability-recovery-regression-bench (customSmallerIsBetter) benchmark result for 0511431bf95ae81b284887100444bd1c560188e0

--- bench/availability-recovery-regression-bench/data.js
+++ bench/availability-recovery-regression-bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1751239728982,
+  "lastUpdate": 1751260145196,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "availability-recovery-regression-bench": [
@@ -61855,6 +61855,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "test-environment",
             "value": 0.2000806886,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "pgherveou@gmail.com",
+            "name": "PG Herveou",
+            "username": "pgherveou"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0511431bf95ae81b284887100444bd1c560188e0",
+          "message": "Add address revive runtime API (#8851)\n\nThis gives an easy way for substrate user to convert an Account ID into\nthe mapped H160 address, by calling a new `address` runtime API\nfunction.\n\n---------\n\nCo-authored-by: cmd[bot] <41898282+github-actions[bot]@users.noreply.github.com>",
+          "timestamp": "2025-06-30T04:07:31Z",
+          "tree_id": "6edc783272386590f5912f28df95c8afc764d3e5",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/0511431bf95ae81b284887100444bd1c560188e0"
+        },
+        "date": 1751260126997,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Sent to peers",
+            "value": 1.6666666666666665,
+            "unit": "KiB"
+          },
+          {
+            "name": "Received from peers",
+            "value": 307203,
+            "unit": "KiB"
+          },
+          {
+            "name": "test-environment",
+            "value": 0.19977409579999997,
+            "unit": "seconds"
+          },
+          {
+            "name": "availability-recovery",
+            "value": 11.350777991300003,
             "unit": "seconds"
           }
         ]
